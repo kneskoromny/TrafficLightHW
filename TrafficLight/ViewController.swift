c0720d229e73df2d5abf4046d114530fadcaea46
kneskoromny <kneskoromny@gmail.com>
2021-03-24 09:44:12 +0300

    Traffic Light is ready

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    
+    @IBOutlet weak var yellowLightView: UIView!
+    
+    @IBOutlet weak var greenLightView: UIView!
+    
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    enum Lights {
+        case noSignal
+        case redSignal
+        case yellowSignal
+        case greenSignal
+    }
+    
+    var currentLight = Lights.noSignal
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = 60
+        redLightView.alpha = 0.3
+        
+        yellowLightView.layer.cornerRadius = 60
+        yellowLightView.alpha = 0.3
+        
+        greenLightView.layer.cornerRadius = 60
+        greenLightView.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 10
+        
     }
 
 
+    @IBAction func startButtonPressed() {
+        
+        startButton.setTitle("NEXT!", for: .normal)
+        
+        switch currentLight {
+            
+        case .redSignal:
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            currentLight = .yellowSignal
+            
+        case .yellowSignal:
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            currentLight = .greenSignal
+            
+        case .greenSignal:
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+            currentLight = .redSignal
+        
+        default:
+            redLightView.alpha = 1
+            currentLight = .redSignal
+            
+        }
+        
+}
+    
+    
+        
+    
 }
 
