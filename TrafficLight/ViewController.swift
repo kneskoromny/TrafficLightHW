9469eb4e5495d9e218a834b45d1c97ebfc429631
kneskoromny <kneskoromny@gmail.com>
2021-03-24 18:31:31 +0300

    minor changes to project

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,48 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+// MARK: - IB Outlets
     @IBOutlet weak var redLightView: UIView!
-    
     @IBOutlet weak var yellowLightView: UIView!
-    
     @IBOutlet weak var greenLightView: UIView!
-    
-    
     @IBOutlet weak var startButton: UIButton!
     
-    enum Lights {
+//    MARK: - Private Properties
+    private enum Lights {
         case noSignal
         case redSignal
         case yellowSignal
         case greenSignal
     }
+    private var currentLight = Lights.noSignal
     
-    var currentLight = Lights.noSignal
+//    MARK: - Life Cycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         redLightView.layer.cornerRadius = 60
-        redLightView.alpha = 0.3
-        
         yellowLightView.layer.cornerRadius = 60
-        yellowLightView.alpha = 0.3
-        
         greenLightView.layer.cornerRadius = 60
-        greenLightView.alpha = 0.3
-        
         startButton.layer.cornerRadius = 10
-        
     }
 
-
+// MARK: - IB Actions
     @IBAction func startButtonPressed() {
         
         startButton.setTitle("NEXT!", for: .normal)
         
         switch currentLight {
-            
         case .redSignal:
             redLightView.alpha = 0.3
             yellowLightView.alpha = 1
@@ -64,7 +54,7 @@ class ViewController: UIViewController {
             greenLightView.alpha = 0.3
             redLightView.alpha = 1
             currentLight = .redSignal
-        
+            
         default:
             redLightView.alpha = 1
             currentLight = .redSignal
